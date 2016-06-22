0baca7dee2959dc66c4bd8bc9edeca88587816c9
GoMapur <mingjian.lu@berkeley.edu>
2016-06-21 20:52:13 -0700

    Continuing beatifying nonhomogeneous code. The code still need testing, noticing that this project needs splitting functionality and refactoring.

--- Python Translation/nonhomogenous.py
+++ Python Translation/nonhomogenous.py
@@ -31,7 +31,7 @@ def non_homogenous(var):
         m12 = [0, m1, m2]
         xx = random.random()  # python random gives a float in [0.0, 1.0)
         # turns out both Matlab and python use deterministic randomgenerators
-        if xx <= m1 / (m1 + m2):
+        if xx <= m1/(m1+m2):
             mm = 0
         else:
             mm = 1
@@ -66,12 +66,12 @@ def non_homogenous(var):
         if a > 1:
             if a < m1 / interval + 2:  # check order of op here
                 i += 1
-                x1 = (a - 1) * interval
+                x1 = (a-1) * interval
                 s += x1
                 x = np.asarray([[s, 2]])
             else:  # not sure if this is inner or outer if CHECK
                 i += 1
-                x1 = (a - m1 / interval - 1) * interval
+                x1 = (a - m1/interval - 1) * interval
                 s += x1
                 x = np.asarray([s, mm % 2 + 1])
                 mm += 1
@@ -94,53 +94,52 @@ def non_homogenous(var):
     j = 1
     extra = []  # using a list will be easier for extra follow periodic code
     t1 = i * H
-    L = np.asarray([[0, x[j - 1, 1]]])
-    if t1 > x[j - 1, 0]:
-        extra[i - 1] = 2
-        h[n1 - 1] = x[j - 1, 0] / 2
-        L[n1, 0] = h[n1 - 1]
-        L[n1, 1] = x[j - 1, 1]
+    L = np.asarray([[0, x[j-1, 1]]])
+    if t1 > x[j-1, 0]:
+        extra[i-1] = 2
+        h[n1-1] = x[j-1, 0] / 2
+        L[n1, 0] = h[n1-1]
+        L[n1, 1] = x[j-1, 1]
         h[n1] = h[n1 - 1]
-        L[n1 + 1, 0] = x[j - 1, 0]
-        L[n1 + 1, 1] = 3
+        L[n1+1, 0] = x[j-1, 0]
+        L[n1+1, 1] = 3
         n1 += 2
     else:
-        while t1 <= x[j - 1, 0]:
-            h[n1 - 1] = H
+        while t1 <= x[j-1, 0]:
+            h[n1-1] = H
             # Begin translation
             L[n1, 0] = i * H
-            if L[n1, 0] == x(j - 1, 0):
+            if L[n1, 0] == x(j-1, 0):
                 # checking is point is interface point?
                 L[n1, 1] = 3
             else:
-                L[n1, 1] = x[j - 1, 1]
+                L[n1, 1] = x[j-1, 1]
             n1 = n1 + 1
             i = i + 1
             t1 = i * H
-        if x[j - 1, 0] < T and L[n1 - 1] != x[j - 1, 0]:
-            extra[i - 1] = 1
-            L[n1, 0] = x[j - 1, 0]
+        if x[j-1, 0] < T and L[n1-1] != x[j-1, 0]:
+            extra[i-1] = 1
+            L[n1, 0] = x[j-1, 0]
             L[n1, 1] = 3
-            h[n1 - 1] = L[n1, 0] - L[n1 - 1, 0]
+            h[n1-1] = L[n1, 0] - L[n1-1, 0]
             n1 += 1
-        if t1 == T and (x[j, 0] - x[j - 1, 0]) < H:
-            extra[i - 1] = extra[i - 1] + 1
+        if t1 == T and (x[j, 0] - x[j-1, 0]) < H:
+            extra[i-1] = extra[i-1] + 1
             j = j + 1
-            L[n1, 0] = (x[j - 1, 0] + x[j - 2, 0]) / 2
-            L[n1, 1] = x[j - 2, 1]
-            h[n1 - 1] = L[n1, 0] - L[n1 - 1, 0]
+            L[n1, 0] = (x[j-1, 0] + x[j-2, 0]) / 2
+            L[n1, 1] = x[j-2, 1]
+            h[n1-1] = L[n1, 0] - L[n1-1, 0]
             n1 = n1 + 1
-            L[n1, 0] = x[j - 1, 0]
+            L[n1, 0] = x[j-1, 0]
             L[n1, 1] = 3
-            h[n1 - 1] = L[n1, 0] - L[n1 - 1, 0]
+            h[n1-1] = L[n1, 0] - L[n1-1, 0]
             n1 = n1 + 1
             i = i + 1
     j = 2
     if x[0, 0] != T:
         while i <= n:
-
-            if t1 > x[j - 1, 0]:
-                extra[i - 1] = extra[i - 1] + 2
+            if t1 > x[j-1, 0]:
+                extra[i-1] = extra[i-1] + 2
                 L[n1, 0] = (x[j - 1, 0] + x[j - 2, 0]) / 2
                 L[n1, 1] = x[j - 1, 1]
                 h[n1 - 1] = L[n1, 0] - L[n1 - 1, 0]
@@ -184,111 +183,86 @@ def non_homogenous(var):
     n1 = n1 - 1
     A = zeros(N * n1, N * n1)
     B = zeros(N * n1, 1)
-    Et12 = zeros(2, 1)
-    Es12 = zeros(2, 1)
-    Q12 = zeros(2, 1)
-    Et12[0] = Et1
-    Et12[1] = Et2
-    Es12[0] = Es1
-    Es12[1] = Es2
-    Q12[0] = Q1
-    Q12[1] = Q2
+    Et12 = [Et1, Et2]
+    Es12 = [Es1, Es2]
+    Q12 = [Q1, Q2]
     minleft = T / 2 - m1 / 2
     maxright = T / 2 + m1 / 2
     for t in arange(1, N / 2).reshape(-1):
         s = (t - 1) * n1
-        A[s, s] = - u[t - 1] * (1 / h[0] + 1 / (h[0] + h[1])) + \
-            Et12[L[0, 1] - 1] - Es12[L[0, 1] - 1] * wt[t - 1] / 2
+        A[s, s] = - u[t - 1] * (1 / h[0] + 1 / (h[0] + h[1])) + Et12[L[0, 1] - 1] - Es12[L[0, 1] - 1] * wt[t - 1] / 2
         A[s, s + 1] = u[t - 1] * (1 / h[0] + 1 / h[1])
         A[s, s + 2] = - u[t - 1] * (h[0] / (h[1] * (h[0] + h[1])))
         B[s] = 0.0
         for i in arange(2, n1 - 1).reshape(-1):
             if L[i - 1, 1] == 3:
                 if i == n1 - 1:
-                    A[s + i - 1, s + i - 1] = - u[t - 1] * (1 / h[i - 1] + 1 / (h[i - 1] + h[i])) + Et12[
-                        L[i, 1] - 1] - Es12[L[i, 1] - 1] * wt[t - 1] / 2
-                    A[s + i - 1, s + i] = u[t - 1] * (1 / h[i - 1] + 1 / h[i])
+                    A[s+i-1, s+i-1] = -u[t-1] * (1/h[i-1] + 1/(h[i-1] + h[i])) + Et12[L[i, 1]-1] - Es12[L[i, 1]-1] * wt[t-1] / 2
+                    A[s+i-1, s+i] = u[t-1] * (1/h[i-1] + 1/h[i])
                     if L[i, 0] >= minleft and L[i, 0] <= maxright:
-                        B[s + i - 1] = u[t - 1] * y_ * \
-                            (h[i - 1] / (h[i] * (h[i - 1] + h[i]))) + \
-                            Q12[L[i, 1] - 1] / 2
+                        B[s+i-1] = u[t-1] * y_ * (h[i-1] / (h[i] * (h[i-1] + h[i]))) + Q12[L[i, 1]-1] / 2
                     else:
-                        B[s + i - 1] = u[t - 1] * y_ * \
-                            (h[i - 1] / (h[i] * (h[i - 1] + h[i])))
+                        B[s + i - 1] = u[t - 1] * y_ * (h[i - 1] / (h[i] * (h[i - 1] + h[i])))
                 else:
                     A[s + i - 1, s + i - 1] = - u[t - 1] * (1 / h[i - 1] + 1 / (h[i - 1] + h[i])) + Et12[
                         L[i, 1] - 1] - Es12[L[i, 1] - 1] * wt[t - 1] / 2
                     A[s + i - 1, s + i] = u[t - 1] * (1 / h[i - 1] + 1 / h[i])
-                    A[s + i - 1, s + i + 1] = - u[t - 1] * \
-                        (h[i - 1] / (h[i] * (h[i - 1] + h[i])))
+                    A[s + i - 1, s + i + 1] = - u[t - 1] * (h[i - 1] / (h[i] * (h[i - 1] + h[i])))
                     if L[i, 0] >= minleft and L[i, 0] <= maxright:
                         B[s + i - 1] = Q12[L[i, 1] - 1] / 2
                     else:
                         B[s + i - 1] = 0.0
             else:
-                A[s + i - 1, s + i - 0] = - u[t - 1] * \
-                    h[i - 1] / (h[i - 0] * (h[i - 0] + h[i - 1]))
+                A[s + i - 1, s + i - 0] = - u[t - 1] * h[i - 1] / (h[i - 0] * (h[i - 0] + h[i - 1]))
                 A[s + i - 1, s + i - 1] = u[t - 1] * (h[i - 1] - h[i - 0]) / (
                     h[i - 1] * h[i - 0]) + Et12[L[i - 1, 1] - 1] - Es12[L[i - 1, 1] - 1] * wt[t - 1] / 2
-                A[s + i - 1, s + i] = u[t - 1] * h[i - 0] / \
-                    (h[i - 1] * (h[i - 0] + h[i - 1]))
+                A[s + i - 1, s + i] = u[t - 1] * h[i - 0] / (h[i - 1] * (h[i - 0] + h[i - 1]))
                 if L[i - 1, 0] >= minleft and L[i - 1, 0] <= maxright:
                     B[s + i - 1] = Q12[L[i - 1, 1] - 1] / 2
                 else:
                     B[s + i - 1] = 0.0
-        A[s + n1 - 1, s + n1 - 0] = - u[t - 1] * \
-            h[n1 - 1] / (h[n1 - 0] * (h[n1 - 0] + h[n1 - 1]))
+        A[s + n1 - 1, s + n1 - 0] = - u[t - 1] * h[n1 - 1] / (h[n1 - 0] * (h[n1 - 0] + h[n1 - 1]))
         A[s + n1 - 1, s + n1 - 1] = u[t - 1] * (h[n1 - 1] - h[n1 - 0]) / (h[n1 - 1] * h[
             n1 - 0]) + Et12[L[n1 - 1, 1] - 1] - Es12[L[n1 - 1, 1] - 1] * wt[t - 1] / 2
-        B[s + n1 - 1] = - u[t - 1] * y_ * h[n1 - 0] / \
-            (h[n1 - 1] * (h[n1 - 0] + h[n1 - 1]))
+        B[s + n1 - 1] = - u[t - 1] * y_ * h[n1 - 0] / (h[n1 - 1] * (h[n1 - 0] + h[n1 - 1]))
         l = copy(t)
         if l == 1 and N > 2:
             for p in arange(l + 1, N / 2).reshape(-1):
                 S = (p - 1) * n1
                 for i in arange(1, n1).reshape(-1):
                     if L[i - 1, 1] == 3:
-                        A[s + i - 1, S + i - 1] = - \
-                            Es12[L[i, 1] - 1] * wt[p - 1] / 2
+                        A[s + i - 1, S + i - 1] = -Es12[L[i, 1] - 1] * wt[p - 1] / 2
                     else:
-                        A[s + i - 1, S + i - 1] = - \
-                            Es12[L[i - 1, 1] - 1] * wt[p - 1] / 2
+                        A[s + i - 1, S + i - 1] = -Es12[L[i - 1, 1] - 1] * wt[p - 1] / 2
         else:
             if l > 1 and N > 2:
                 for p in arange(1, l - 1).reshape(-1):
                     S = (p - 1) * n1
                     for i in arange(1, n1).reshape(-1):
                         if L[i - 1, 1] == 3:
-                            A[s + i - 1, S + i - 1] = - \
-                                Es12[L[i, 1] - 1] * wt[p - 1] / 2
+                            A[s + i - 1, S + i - 1] = -Es12[L[i, 1] - 1] * wt[p - 1] / 2
                         else:
-                            A[s + i - 1, S + i - 1] = - \
-                                Es12[L[i - 1, 1] - 1] * wt[p - 1] / 2
+                            A[s + i - 1, S + i - 1] = -Es12[L[i - 1, 1] - 1] * wt[p - 1] / 2
                 for p in arange(l + 1, N / 2).reshape(-1):
                     S = (p - 1) * n1
                     for i in arange(1, n1).reshape(-1):
                         if L[i - 1, 1] == 3:
-                            A[s + i - 1, S + i - 1] = - \
-                                Es12[L[i, 1] - 1] * wt[p - 1] / 2
+                            A[s + i - 1, S + i - 1] = -Es12[L[i, 1] - 1] * wt[p - 1] / 2
                         else:
-                            A[s + i - 1, S + i - 1] = - \
-                                Es12[L[i - 1, 1] - 1] * wt[p - 1] / 2
+                            A[s + i - 1, S + i - 1] = -Es12[L[i - 1, 1] - 1] * wt[p - 1] / 2
         a = 0
         for p in arange(1, N / 2).reshape(-1):
             S = (N / 2 + p - 1) * n1
             for i in arange(2, n1).reshape(-1):
                 if L[i - 1, 1] == 3:
-                    A[s + i - 1, S + i - 0] = - \
-                        Es12[L[i, 1] - 1] * wt[N / 2 + p - 1] / 2
+                    A[s + i - 1, S + i - 0] = -Es12[L[i, 1] - 1] * wt[N / 2 + p - 1] / 2
                 else:
-                    A[s + i - 1, S + i - 0] = - \
-                        Es12[L[i - 1, 1] - 1] * wt[N / 2 + p - 1] / 2
+                    A[s + i - 1, S + i - 0] = -Es12[L[i - 1, 1] - 1] * wt[N / 2 + p - 1] / 2
             a = a + (Es12[L[0, 1] - 1] * wt[N / 2 + p - 1] * yo / 2)
         B[s] = B[s] + a
     for t in arange(N / 2 + 1, N).reshape(-1):
         s = (t - 1) * n1
-        A[s, s] = u[t - 1] * (h[1] - h[0]) / (h[1] * h[0]) + \
-            Et12[L[0, 1] - 1] - Es12[L[0, 1] - 1] * wt[t - 1] / 2
+        A[s, s] = u[t - 1] * (h[1] - h[0]) / (h[1] * h[0]) + Et12[L[0, 1] - 1] - Es12[L[0, 1] - 1] * wt[t - 1] / 2
         A[s, s + 1] = u[t - 1] * h[0] / (h[1] * (h[0] + h[1]))
         B[s] = u[t - 1] * yo * h[1] / (h[0] * (h[0] + h[1]))
         for i in arange(2, n1 - 1).reshape(-1):
@@ -296,43 +270,33 @@ def non_homogenous(var):
                 if i == 2:
                     A[s + i - 1, s + i - 1] = u[t - 1] * (1 / h[i - 1] + 1 / (h[i - 1] + h[i - 0])) + Et12[
                         L[i - 1, 1] - 1] - Es12[L[i - 1, 1] - 1] * wt[t - 1] / 2
-                    A[s + i - 1, s + i - 0] = - u[t - 1] * \
-                        (1 / h[i - 1] + 1 / h[i - 0])
+                    A[s + i - 1, s + i - 0] = - u[t - 1] * (1 / h[i - 1] + 1 / h[i - 0])
                     if L[i - 1, 0] >= minleft and L[i - 1, 0] <= maxright:
-                        B[s + i - 1] = - u[t - 1] * yo * \
-                            (h[i - 1] / (h[i - 0] * (h[i - 1] + h[i - 0]))
-                             ) + Q12[L[i - 1, 1] - 1] / 2
+                        B[s + i - 1] = - u[t - 1] * yo * (h[i - 1] / (h[i - 0] * (h[i - 1] + h[i - 0]))) + Q12[L[i - 1, 1] - 1] / 2
                     else:
-                        B[s + i - 1] = - u[t - 1] * yo * \
-                            (h[i - 1] / (h[i - 0] * (h[i - 1] + h[i - 0])))
+                        B[s + i - 1] = - u[t - 1] * yo * (h[i - 1] / (h[i - 0] * (h[i - 1] + h[i - 0])))
                 else:
                     A[s + i - 1, s + i - 1] = u[t - 1] * (1 / h[i - 1] + 1 / (h[i - 1] + h[i - 0])) + Et12[
                         L[i - 1, 1] - 1] - Es12[L[i - 1, 1] - 1] * wt[t - 1] / 2
-                    A[s + i - 1, s + i - 0] = - u[t - 1] * \
-                        (1 / h[i - 1] + 1 / h[i - 0])
-                    A[s + i - 1, s + i - 3] = u[t - 1] * \
-                        (h[i - 1] / (h[i - 0] * (h[i - 1] + h[i - 0])))
+                    A[s + i - 1, s + i - 0] = - u[t - 1] * (1 / h[i - 1] + 1 / h[i - 0])
+                    A[s + i - 1, s + i - 3] = u[t - 1] * (h[i - 1] / (h[i - 0] * (h[i - 1] + h[i - 0])))
                     if L[i - 1, 0] >= minleft and L[i - 1, 0] <= maxright:
                         B[s + i - 1] = Q12[L[i - 1, 1] - 1] / 2
                     else:
                         B[s + i - 1] = 0.0
             else:
-                A[s + i - 1, s + i - 0] = - u[t - 1] * \
-                    h[i] / (h[i - 1] * (h[i - 1] + h[i]))
+                A[s + i - 1, s + i - 0] = - u[t - 1] * h[i] / (h[i - 1] * (h[i - 1] + h[i]))
                 A[s + i - 1, s + i - 1] = u[t - 1] * (h[i] - h[i - 1]) / (h[i] * h[i - 1]) + Et12[
                     L[i, 1] - 1] - Es12[L[i, 1] - 1] * wt[t - 1] / 2
-                A[s + i - 1, s + i] = u[t - 1] * \
-                    h[i - 1] / (h[i] * (h[i - 1] + h[i]))
+                A[s + i - 1, s + i] = u[t - 1] * h[i - 1] / (h[i] * (h[i - 1] + h[i]))
                 if L[i, 0] >= minleft and L[i, 0] <= maxright:
                     B[s + i - 1] = Q12[L[i, 1] - 1] / 2
                 else:
                     B[s + i - 1] = 0.0
         A[s + n1 - 1, s + n1 - 1] = u[t - 1] * (1 / h[n1 - 1] + 1 / (h[n1 - 1] + h[n1 - 0])) + Et12[
             L[n1 - 1, 1] - 1] - Es12[L[n1 - 1, 1] - 1] * wt[t - 1] / 2
-        A[s + n1 - 1, s + n1 - 0] = - u[t - 1] * \
-            (1 / h[n1 - 1] + 1 / h[n1 - 0])
-        A[s + n1 - 1, s + n1 - 3] = u[t - 1] * \
-            (h[n1 - 1] / (h[n1 - 0] * (h[n1 - 1] + h[n1 - 0])))
+        A[s + n1 - 1, s + n1 - 0] = - u[t - 1] * (1 / h[n1 - 1] + 1 / h[n1 - 0])
+        A[s + n1 - 1, s + n1 - 3] = u[t - 1] * (h[n1 - 1] / (h[n1 - 0] * (h[n1 - 1] + h[n1 - 0])))
         B[s + n1 - 1] = 0.0
         l = copy(t)
         if l == N / 2 + 1 and N > 2:
@@ -340,31 +304,25 @@ def non_homogenous(var):
                 S = (p - 1) * n1
                 for i in arange(1, n1).reshape(-1):
                     if L[i, 1] == 3:
-                        A[s + i - 1, S + i - 1] = - \
-                            Es12[L[i - 1, 1] - 1] * wt[p - 1] / 2
+                        A[s + i - 1, S + i - 1] = -Es12[L[i - 1, 1] - 1] * wt[p - 1] / 2
                     else:
-                        A[s + i - 1, S + i - 1] = - \
-                            Es12[L[i, 1] - 1] * wt[p - 1] / 2
+                        A[s + i - 1, S + i - 1] = -Es12[L[i, 1] - 1] * wt[p - 1] / 2
         else:
             if l > N / 2 + 1 and N > 2:
                 for p in arange(N / 2 + 1, l - 1).reshape(-1):
                     S = (p - 1) * n1
                     for i in arange(1, n1).reshape(-1):
                         if L[i, 1] == 3:
-                            A[s + i - 1, S + i - 1] = - \
-                                Es12[L[i - 1, 1] - 1] * wt[p - 1] / 2
+                            A[s + i - 1, S + i - 1] = -Es12[L[i - 1, 1] - 1] * wt[p - 1] / 2
                         else:
-                            A[s + i - 1, S + i - 1] = - \
-                                Es12[L[i, 1] - 1] * wt[p - 1] / 2
+                            A[s + i - 1, S + i - 1] = -Es12[L[i, 1] - 1] * wt[p - 1] / 2
                 for p in arange(l + 1, N).reshape(-1):
                     S = (p - 1) * n1
                     for i in arange(1, n1).reshape(-1):
                         if L[i, 1] == 3:
-                            A[s + i - 1, S + i - 1] = - \
-                                Es12[L[i - 1, 1] - 1] * wt[p - 1] / 2
+                            A[s + i - 1, S + i - 1] = -Es12[L[i - 1, 1] - 1] * wt[p - 1] / 2
                         else:
-                            A[s + i - 1, S + i - 1] = - \
-                                Es12[L[i, 1] - 1] * wt[p - 1] / 2
+                            A[s + i - 1, S + i - 1] = -Es12[L[i, 1] - 1] * wt[p - 1] / 2
         a = 0
         for p in arange(1, N / 2).reshape(-1):
             S = (p - 1) * n1
