cd0f89150add9397cdde76f4f3ccae8503233390
GoMapur <mingjian.lu@berkeley.edu>
2016-10-03 11:00:27 -0700

    Finish fixing homogeneous benchmark

--- Python Refactoring/SolverTest.py
+++ Python Refactoring/SolverTest.py
@@ -4,5 +4,5 @@ from OneDGridModelSolver import *
 
 materials1 = Model_Material(thickness = 1.0, cross_section = 1.0, scattering_ratio = 1.0, homogeneous_isotropic_source = 0.0)
 materials2 = Model_Material(thickness = 1.0, cross_section = 1.0, scattering_ratio = 1.0, homogeneous_isotropic_source = 0.0)
-bench = Model_1D_Periodic_Finite_Step_Benchmark(total_len = 10.0, point_num = 100, boundary_cond = [1.0, 0.0], materials = [materials1, materials2], gauss_discrete_direction_num = 2)
+bench = Model_1D_Homogeneous_Finite_Step_Benchmark(total_len = 4.0, point_num = 20, boundary_cond = [1.0, 0.0], materials = [materials1, materials2], gauss_discrete_direction_num = 4)
 bench.benchmark()
